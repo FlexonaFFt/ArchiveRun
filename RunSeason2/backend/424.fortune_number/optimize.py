2f772111a65d1ea8d113be3cf75499bae0f5bf80
FlexonaFFt <flexonafft@bk.ru>
2024-08-20 14:40:41 +0300

    --load day24 2

--- RunSeason2/backend/424.fortune_number/optimize.py
+++ RunSeason2/backend/424.fortune_number/optimize.py
@@ -1,26 +1,26 @@
-def next_happy_number(number):
-    n = len(number)
-    polovina = len(number) // 2
+# Решение ломается на 12 тесте
+def find_closest_number(input_number):
+    def sum_of_half_digits(number):
+        half = len(number) // 2
+        first_half = sum(int(digit) for digit in number[:half])
+        second_half = sum(int(digit) for digit in number[half:])
+        return first_half, second_half
 
+    def is_sum_equal(number):
+        first_half, second_half = sum_of_half_digits(number)
+        return first_half == second_half
+
+    number = str(int(input_number) + 1).zfill(len(input_number))
     while True:
-        number = str(int(number[-1]) + 1) + number[:-1]
-        if len(number) > polovina * 2:
-            number = str("0" * (polovina * 2 - 1) + "1")
-
-        left_sum = 0
-        for digit in number[:polovina]:
-            left_sum += int(digit)
-
-        right_sum = 0
-        for digit in number[polovina:]:
-            right_sum += int(digit)
-
-        if left_sum == right_sum and left_sum > 0:
+        if number != '0' * len(input_number) and is_sum_equal(number):
             return number
+        number = str(int(number) + 1).zfill(len(input_number))
+        if len(number) > len(input_number):
+            number = '0' * len(input_number)
 
 def main():
     number = str(input())
-    print(next_happy_number(number))
+    print(find_closest_number(number))
 
 if __name__ == '__main__':
     main()
