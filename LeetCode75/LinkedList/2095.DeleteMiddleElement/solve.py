50920a51e6e1e8de7623df0d043e217a06eb7b35
FlexonaFFt <flexonafft@bk.ru>
2025-01-25 19:57:07 +0300

    podgotovka day15 9

---
+++ LeetCode75/LinkedList/2095.DeleteMiddleElement/solve.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    from typing import Optional
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast, slow, prev = head, head, None
+        if not head or not head.next:
+            return None
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        prev.next = slow.next
+        return head
