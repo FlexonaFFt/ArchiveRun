fc2150107923678e19cee520046869057420ac18
FlexonaFFt <flexonafft@bk.ru>
2025-01-27 20:07:07 +0300

    podgotovka day17 5

---
+++ LeetCodeTasks/12.IntegerToRoman/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        val = [(1000, "M"), (900, "CM"), (500, "D"),
+            (400, "CD"), (100, "C"), (90, "XC"),
+            (50, "L"), (40, "XL"), (10, "X"),
+            (9, "IX"), (5, "V"), (4, "IV"),
+            (1, "I")]
+
+        result = []
+        for value, symbol in val:
+            while num >= value:
+                result.append(symbol)
+                num -= value
+        return ''.join(result)
+
+
+def main():
+    num1, num2, num3 = 3749, 58, 1994
+    solution = Solution()
+    print(solution.intToRoman(num1))
+    print(solution.intToRoman(num2))
+    print(solution.intToRoman(num3))
+
+if __name__ == '__main__':
+    main()
