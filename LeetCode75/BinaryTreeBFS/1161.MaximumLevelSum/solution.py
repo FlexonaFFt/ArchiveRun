79e9f6198da4158beea1f76689009236e75b8d94
FlexonaFFt <flexonafft@bk.ru>
2025-01-28 12:45:24 +0300

    podgotovka day18 2

---
+++ LeetCode75/BinaryTreeBFS/1161.MaximumLevelSum/solution.py
@@ -0,0 +1,32 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+from collections import deque
+from typing import Optional
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        max_sum, max_level, level = float('-inf'), 0, 0
+        queue = deque([root])
+        while queue:
+            level += 1
+            level_size = len(queue)
+            current_sum = 0
+            for _ in range(level_size):
+                node = queue.popleft()
+                current_sum += node.val
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            if current_sum > max_sum:
+                max_sum = current_sum
+                max_level = level
+        return max_level
