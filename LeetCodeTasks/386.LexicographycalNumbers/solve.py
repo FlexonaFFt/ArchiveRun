1a0b33286afd280525b0c11a3402a3503d5162fb
FlexonaFFt <flexonafft@bk.ru>
2025-02-18 18:59:06 +0300

    podgotovka day31 3

--- LeetCodeTasks/386.LexicographycalNumbers/solve.py
+++ LeetCodeTasks/386.LexicographycalNumbers/solve.py
@@ -1,13 +1,23 @@
 class Solution:
     from typing import List
     def lexicalOrder(self, n: int) -> List[int]:
-        unsortedList = []
-        for i in range(1, n + 1):
-            unsortedList.append(i)
-        for element in unsortedList:
-            if ord(element)
+        result, stack = [], []
+        for i in range(9, 0, -1):
+            if i <= n:
+                stack.append(i)
 
+        while stack:
+            current = stack.pop()
+            result.append(current)
+            for i in range(9, -1, -1):
+                next_num = current * 10 + i
+                if next_num <= n:
+                    stack.append(next_num)
 
+        return result
+
+# Runtime 55 ms, 23.20 %
+# Memory 21.28 mb, 74.96 %
 def main():
     solve = Solution()
     print(solve.lexicalOrder(n=13))
