40aedbab4f4440935202405cf8a048cec1cf2035
FlexonaFFt <flexonafft@bk.ru>
2025-02-21 22:38:52 +0300

    podgotovka day33 3

---
+++ LeetCodeTasks/100.SameTree/solve.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    from typing import Optional
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+# Runtime 0 ms, 100 %
+# Memory 17.64 mb, 91.01 %
