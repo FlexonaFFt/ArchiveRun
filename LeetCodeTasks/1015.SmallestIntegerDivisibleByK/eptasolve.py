d1fc601f690a545ba9423c87ad86be6103661884
FlexonaFFt <flexonafft@bk.ru>
2025-03-18 10:04:20 +0300

    podgotovka day48 1

---
+++ LeetCodeTasks/1015.SmallestIntegerDivisibleByK/eptasolve.py
@@ -0,0 +1,22 @@
+class Solution:
+    def smallestRepunitDivByK(self, k: int) -> int:
+        if k % 10 not in {1, 3, 7, 9}: return -1
+        mod, mod_set = 0, set()
+        for length in range(1, k + 1):
+            mod = (10 * mod + 1) % k
+            if mod == 0:
+                return length
+            if mod in mod_set:
+                return -1
+            mod_set.add(mod)
+        return -1
+
+# Runtime 11 ms, 73.11 %
+# Memory 21.82 mb, 6.06 %
+def main():
+    epta = Solution()
+    print(epta.smallestRepunitDivByK(k=1))
+    print(epta.smallestRepunitDivByK(k=2))
+    print(epta.smallestRepunitDivByK(k=3))
+
+main()
