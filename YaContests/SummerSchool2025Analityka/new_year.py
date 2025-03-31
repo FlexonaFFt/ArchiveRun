5d420b535c626503fddacca74a039c4677c79ef4
FlexonaFFt <flexonafft@bk.ru>
2025-03-31 14:32:02 +0300

    new_year WA

---
+++ YaContests/SummerSchool2025Analityka/new_year.py
@@ -0,0 +1,78 @@
+from typing import List
+import sys
+
+class Solution:
+    def solveFunction(self, n: int, nlst: List[str], m: int, mlst: List[str]) -> None:
+        def time_to_seconds(time_str):
+            hours, minutes, seconds = map(int, time_str.split(":"))
+            return hours * 3600 + minutes * 60 + seconds
+
+        electric_trains = []
+        for i in range(n):
+            train_id, departure_time, arrival_time, cost = nlst[i].split()
+            electric_trains.append({
+                "id": train_id,
+                "departure": time_to_seconds(departure_time),
+                "arrival": time_to_seconds(arrival_time),
+                "cost": int(cost)
+            })
+
+        diesel_trains = []
+        for i in range(m):
+            data = mlst[i].split()
+            train_id, departure_time, arrival_time = data[:3]
+            diesel_trains.append({
+                'id': train_id,
+                'departure': time_to_seconds(departure_time),
+                'arrival': time_to_seconds(arrival_time)
+            })
+
+        # Поиск оптимального маршрута
+        best_route, min_cost = None, float("inf")
+        for electric in electric_trains:
+            for diesel in diesel_trains:
+                if electric['arrival'] + 15 * 60 >= diesel['departure']:
+                    total_cost = electric['cost']
+                    if total_cost < min_cost or \
+                       (total_cost == min_cost and diesel['arrival'] < best_route[1]['arrival']) or \
+                       (total_cost == min_cost and diesel['arrival'] == best_route[1]['arrival'] and \
+                        electric['departure'] < best_route[0]['departure']):
+                        min_cost = total_cost
+                        best_route = (electric, diesel)
+
+        if best_route:
+            print(best_route[0]['id'])
+            print(best_route[1]['id'])
+
+
+def test():
+    solve = Solution()
+    n, m = 3, 3
+    nlist = ['6073 00:25:00 02:36:00 468',
+        '083Y 06:05:05 07:58:59 1147',
+        '7203 08:36:00 10:30:00 575']
+    mlist = ['6731 10:05:00 11:24:00 246',
+        'ABCDE 02:50:59 03:15:00 100',
+        'X 02:51:00 03:25:00 200']
+    solve.solveFunction(n, nlist, m, mlist)
+
+
+def main():
+    solve = Solution()
+    nlist, mlist = [], []
+
+    n = int(input())
+    for _ in range(n):
+        ndata = input().strip()
+        nlist.append(ndata)
+
+    m = int(input())
+    for _ in range(m):
+        mdata = input().strip()
+        mlist.append(mdata)
+
+    solve.solveFunction(n, nlist, m, mlist)
+
+
+if __name__ == '__main__':
+    test()
