6e2e17dc8ac1b6192fe1d039ad23faf0fb2d7a20
FlexonaFFt <flexonafft@bk.ru>
2025-04-04 07:28:24 +0300

    daily April 4

---
+++ LeetCodeTasks/2874.MaximumValueOrderedTripletII/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maximumTripletValue(self, nums: list[int]) -> int:
+        n = len(nums)
+        if n < 3:
+            return 0
+
+        left_max = [0] * n
+        left_max[0] = nums[0]
+        for i in range(1, n):
+            left_max[i] = max(left_max[i - 1], nums[i])
+
+        right_max = [0] * n
+        right_max[-1] = nums[-1]
+        for i in range(n - 2, -1, -1):
+            right_max[i] = max(right_max[i + 1], nums[i])
+
+        ans = 0
+        for i in range(1, n - 1):
+            left = left_max[i - 1]
+            right = right_max[i + 1]
+            ans = max(ans, (left - nums[i]) * right)
+        return ans
+
+
+def main():
+    solution = Solution()
+    print(solution.maximumTripletValue(nums=[12,6,1,2,7]))
+    print(solution.maximumTripletValue(nums=[1,10,3,4,19]))
+    print(solution.maximumTripletValue(nums=[1,2,3]))
+
+main()
