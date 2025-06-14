d889a80abaabe358edf4fbf66cff533da2a4e38b
FlexonaFFt <flexonafft@bk.ru>
2025-06-14 22:39:26 +0300

    reshil Valid Sudoku

---
+++ LeetCodeTasks/36.ValidSudoku/solution.py
@@ -0,0 +1,46 @@
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+                if num ==".": continue
+                if num in rows[i]: return False
+                rows[i].add(num)
+                if num in cols[j]: return False
+                cols[j].add(num)
+
+                box_index = (i // 3) * 3 + (j // 3)
+                if num in boxes[box_index]: return False
+                boxes[box_index].add(num)
+
+        return True
+
+# Runtime 5 ms, 48.67 %
+# Memory 18.00 mb, 24.91 %
+def test():
+    solve = Solution()
+    print(solve.isValidSudoku([["5","3",".",".","7",".",".",".","."]
+    ,["6",".",".","1","9","5",".",".","."]
+    ,[".","9","8",".",".",".",".","6","."]
+    ,["8",".",".",".","6",".",".",".","3"]
+    ,["4",".",".","8",".","3",".",".","1"]
+    ,["7",".",".",".","2",".",".",".","6"]
+    ,[".","6",".",".",".",".","2","8","."]
+    ,[".",".",".","4","1","9",".",".","5"]
+    ,[".",".",".",".","8",".",".","7","9"]]))
+
+    print(solve.isValidSudoku([["8","3",".",".","7",".",".",".","."]
+    ,["6",".",".","1","9","5",".",".","."]
+    ,[".","9","8",".",".",".",".","6","."]
+    ,["8",".",".",".","6",".",".",".","3"]
+    ,["4",".",".","8",".","3",".",".","1"]
+    ,["7",".",".",".","2",".",".",".","6"]
+    ,[".","6",".",".",".",".","2","8","."]
+    ,[".",".",".","4","1","9",".",".","5"]
+    ,[".",".",".",".","8",".",".","7","9"]]))
+
+if __name__ == '__main__': test()
