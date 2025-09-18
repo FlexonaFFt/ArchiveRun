8fd84a43af167e96f00957a6d278dff3bdf51bc7
FlexonaFFt <flexonafft@bk.ru>
2025-09-18 22:22:35 +0300

    daily September 18

---
+++ LeetCodeTasks/3408.DesignTaskManager/solve.py
@@ -0,0 +1,33 @@
+import heapq
+
+class TaskManager:
+    def __init__(self, tasks):
+        self.task_info = {}  
+        self.heap = []
+        for userId, taskId, priority in tasks:
+            self.task_info[taskId] = (userId, priority)
+            heapq.heappush(self.heap, (-priority, -taskId, taskId))
+
+    def add(self, userId, taskId, priority):
+        self.task_info[taskId] = (userId, priority)
+        heapq.heappush(self.heap, (-priority, -taskId, taskId))
+
+    def edit(self, taskId, newPriority):
+        userId, _ = self.task_info[taskId]
+        self.task_info[taskId] = (userId, newPriority)
+        heapq.heappush(self.heap, (-newPriority, -taskId, taskId))
+
+    def rmv(self, taskId):
+        if taskId in self.task_info:
+            del self.task_info[taskId]
+
+    def execTop(self):
+        while self.heap:
+            priority, neg_taskId, taskId = heapq.heappop(self.heap)
+            if taskId in self.task_info:
+                userId, cur_priority = self.task_info[taskId]
+                if -priority == cur_priority:
+                    del self.task_info[taskId]
+                    return userId
+        return -1
+    
