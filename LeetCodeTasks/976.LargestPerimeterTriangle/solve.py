fdeba9cf8b0a3d4a1f26945a1b414a095d6bb8e1
FlexonaFFt <flexonafft@bk.ru>
2025-09-29 00:08:17 +0300

    daily September 28

---
+++ LeetCodeTasks/976.LargestPerimeterTriangle/solve.py
@@ -0,0 +1,6 @@
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        for x,y,z in zip(nums, nums[1:], nums[2:]):
+            if x < y + z: return x + y + z
+        return 0
