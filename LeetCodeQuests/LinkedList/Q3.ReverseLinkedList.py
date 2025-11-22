ae1e10a72b19b112efa29f27123eec3572203725
FlexonaFFt <flexonafft@bk.ru>
2025-11-22 17:29:04 +0300

    quest LinkedList OK

---
+++ LeetCodeQuests/LinkedList/Q3.ReverseLinkedList.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None: return head
+
+        new_head = self.reverseList(head.next)
+        head.next.next, head.next = head, None
+        return new_head
