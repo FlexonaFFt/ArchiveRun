97836abada0e6ecd9c3b6d97b6cedc2a5fbe0a8b
FlexonaFFt <flexonafft@bk.ru>
2026-02-05 16:33:23 +0300

    daily February 5

---
+++ LeetCodeTasks/3379.TransformedArray/solve.py
@@ -0,0 +1,5 @@
+from typing import List 
+
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        return [nums[(i+nums[i])%len(nums)] for i in range(len(nums))]
