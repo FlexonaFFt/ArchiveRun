a1aa644d966c9e1c715fc15dcd6f62a0405c038f
FlexonaFFt <flexonafft@bk.ru>
2026-02-26 10:56:41 +0300

    daily February 26

---
+++ LeetCodeTasks/1404.NumberOfSteps/solve.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numSteps(self, s: str) -> int:
+        num, steps = int(s, 2), 0 
+        while num != 1:
+            if num % 2 == 0: num //= 2
+            else: num += 1
+            steps += 1
+        return steps
