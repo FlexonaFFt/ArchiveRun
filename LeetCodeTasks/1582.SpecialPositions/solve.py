23604319f5d0d6936c8cf881f5abaf498a352c86
FlexonaFFt <flexonafft@bk.ru>
2026-03-04 09:49:07 +0300

    daily March 4

---
+++ LeetCodeTasks/1582.SpecialPositions/solve.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numSpecial(self, mat: List[List[int]]) -> int:
+        
+        m, n, ans = len(mat), len(mat[0]), 0
+        for row in range(m):
+            for col in range(n):
+                if mat[row][col] == 0:
+                    continue
+
+                good = True 
+                for r in range(m):
+                    if r != row and mat[r][col] == 1:
+                        good = False 
+                        break 
+
+                for c in range(n):
+                    if c != col and mat[row][c] == 1:
+                        good = False 
+                        break 
+                
+                if good: ans += 1
+
+        return ans 
