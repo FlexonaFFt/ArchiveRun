7a06e4afc94bbebed1ac2d9616c9ae0d6c5d5496
FlexonaFFt <flexonafft@bk.ru>
2026-04-13 21:12:07 +0300

    daily April 13

---
+++ LeetCodeTasks/1848.MinimumDistance/solve.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        result = len(nums)
+        for i, num in enumerate(nums):
+            if num == target:
+                result = min(result, abs(i - start))
+
+        return result 
