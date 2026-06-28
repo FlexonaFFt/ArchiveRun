13bc4f6b7ada9b372a0e9af42fabb5905dc55d82
FlexonaFFt <flexonafft@bk.ru>
2026-06-28 10:23:40 +0300

    daily June 28

---
+++ platforms/leetcode/tasks/1846.MaximumElementAfter/solution.py
@@ -0,0 +1,10 @@
+from typing import List 
+
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        arr.sort()
+        ans = 0
+
+        for x in arr:
+            if x > ans: ans += 1
+        return ans 
