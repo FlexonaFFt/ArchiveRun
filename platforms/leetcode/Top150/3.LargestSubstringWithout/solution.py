0fea3dc765c74f65e5342cdf250c604c8e84ec2b
FlexonaFFt <flexonafft@bk.ru>
2026-06-28 10:30:47 +0300

    daily Top150 June 28

---
+++ platforms/leetcode/Top150/3.LargestSubstringWithout/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left, best, curr = 0, 0, {}
+
+        for right, value in enumerate(s):
+            curr[value] = curr.get(value, 0) + 1
+            while curr[value] > 1:
+                curr[s[left]] -= 1
+                left += 1
+
+            best = max(best, right - left + 1)
+
+        return best 
