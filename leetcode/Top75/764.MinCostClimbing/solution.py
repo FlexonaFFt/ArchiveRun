c013f37a9f3ec79378e983fa78970b3cdb060239
FlexonaFFt <flexonafft@bk.ru>
2026-07-06 15:05:21 +0300

    daily Top75 July 6 x2

---
+++ leetcode/Top75/764.MinCostClimbing/solution.py
@@ -0,0 +1,9 @@
+from typing import List 
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        a = b = 0 
+        for c in cost: 
+            a, b = b, c + min(a, b)
+
+        return min(a, b)
