0e816674bb1aecca99dd269af00850fcbac0faa8
FlexonaFFt <flexonafft@bk.ru>
2026-07-07 15:02:05 +0300

    daily July 7

---
+++ leetcode/tasks/3754.ConcatenateNonZero/solution.py
@@ -0,0 +1,5 @@
+class Solution:
+    def sumAndMultiply(self, n: int) -> int:
+        nums = [x for x in str(n) if x != '0']
+        if not nums: return 0
+        return int(''.join(nums)) * sum(int(x) for x in nums) 
